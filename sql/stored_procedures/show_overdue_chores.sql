d86b28dfcbf488b82300fa58d1c198bbe93580fd
karldickman <karldickman@gmail.com>
2017-01-14 12:07:49 -0800

    Added code for show_overdue_chores procedure.

---
+++ sql/stored_procedures/show_overdue_chores.sql
@@ -0,0 +1,71 @@
+USE chores;
+DROP procedure IF EXISTS show_overdue_chores;
+
+DELIMITER $$
+USE chores$$
+CREATE DEFINER=root@localhost PROCEDURE show_overdue_chores(from_inclusive DATETIME, until_inclusive DATETIME)
+BEGIN
+	SET from_inclusive = COALESCE(from_inclusive, '1989-02-09');
+    SET @minutes_format = '%i:%S';
+    SET @hours_format = '%H:%i:%S';
+	SELECT chore
+			, due_date
+            , last_completed
+            , TIME_FORMAT(completed_minutes, @minutes_format) AS completed_minutes
+            , TIME_FORMAT(remaining_minutes, @minutes_format) AS remaining_minutes
+			, TIME_FORMAT(remaining_minutes + 1.282 * stdev_duration_minutes, @minutes_format) AS 90% CI UB
+		FROM report_incomplete_chores
+		WHERE due_date <= until_inclusive
+			AND chore NOT IN (SELECT chore
+				FROM chores
+                NATURAL JOIN chore_categories
+				NATURAL JOIN categories
+                WHERE category = 'meals');
+	SELECT category
+		, SUM(number_of_chores) AS number_of_chores
+		, TIME_FORMAT(SUM(backlog_hours), @hours_format) AS backlog_hours
+        , TIME_FORMAT(SUM(stdev_backlog_hours), @hours_format) AS stdev_backlog_hours
+        , TIME_FORMAT(SUM(backlog_hours) + 1.282 * SUM(stdev_backlog_hours), @hours_format) AS 90% CI UB
+	FROM (SELECT category
+			, COUNT(incomplete_chores_progress.chore_id) AS number_of_chores
+			, SUM(CASE
+				WHEN remaining_minutes > 0
+					THEN remaining_minutes
+					ELSE 0
+				END) / 60 AS backlog_hours
+			, SQRT(SUM(CASE
+				WHEN remaining_minutes > 0
+					THEN POWER(stdev_duration_minutes, 2)
+					ELSE 0
+				END)) / 60 AS stdev_backlog_hours
+		FROM incomplete_chores_progress
+        LEFT OUTER JOIN chore_categories
+			ON incomplete_chores_progress.chore_id = chore_categories.chore_id
+		LEFT OUTER JOIN categories
+			ON chore_categories.category_id = categories.category_id
+		WHERE due_date BETWEEN from_inclusive AND until_inclusive
+        GROUP BY category
+	UNION
+	SELECT category
+			, COUNT(never_measured_chores_progress.chore_id) AS number_of_chores
+			, SUM(CASE
+				WHEN remaining_minutes > 0
+					THEN remaining_minutes
+					ELSE 0
+				END) / 60 AS backlog_hours
+			, SUM(CASE
+				WHEN remaining_minutes > 0
+					THEN stdev_duration_minutes
+					ELSE 0
+				END) / 60 AS stdev_backlog_hours
+		FROM never_measured_chores_progress
+        LEFT OUTER JOIN chore_categories
+			ON never_measured_chores_progress.chore_id = chore_categories.chore_id
+		LEFT OUTER JOIN categories
+			ON chore_categories.category_id = categories.category_id
+		WHERE due_date BETWEEN from_inclusive AND until_inclusive
+        GROUP BY category) AS backlog_calculations
+	GROUP BY category;
+END$$
+
+DELIMITER ;
