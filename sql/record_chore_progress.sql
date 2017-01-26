59a11dfe56ad7ad056e85c04bc27bff7dca9f78a
karldickman <karldickman@gmail.com>
2017-01-25 18:13:59 -0800

    deleted:    record_chore_progress.sql     Removed useless query template.

--- sql/record_chore_progress.sql
+++
@@ -1,6 +0,0 @@
-ROLLBACK;
-START TRANSACTION;
-SET @is_completed = TRUE;
-CALL complete_chore(<{chore_name}>, <{when_completed}>, <{chore_due_date}>, FALSE, <{minutes FLOAT}>, <{seconds FLOAT}>, @is_completed, @chore_completion_id, @chore_session_id);
-CALL show_completed_chore(@chore_completion_id);
-/* COMMIT */
