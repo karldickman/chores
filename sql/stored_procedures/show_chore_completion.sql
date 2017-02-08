80d4321f16404f46c7798db3b4996a457914c77f
karldickman <karldickman@gmail.com>
2017-02-07 19:16:11 -0800

    modified:   show_chore_completion.sql     Avoid blank record when there are child chore completions that have     sessions.

--- sql/stored_procedures/show_chore_completion.sql
+++ sql/stored_procedures/show_chore_completion.sql
@@ -5,28 +5,31 @@ DELIMITER $$
 USE chores$$
 CREATE PROCEDURE show_chore_completion(chore_completion_id_to_show INT)
 BEGIN
-	SELECT chore
+	SELECT chore_completions.chore_completion_id
+			, chore
 			, chore_session_id
 			, duration_minutes
 			, when_completed
 			, when_recorded
 		FROM chore_completions
-		NATURAL JOIN chores
-		LEFT OUTER JOIN chore_sessions
-			ON chore_completions.chore_completion_id = chore_sessions.chore_completion_id
-		WHERE chore_completions.chore_completion_id = chore_completion_id_to_show
-	UNION
-    SELECT chore
-			, chore_session_id
-			, duration_minutes
-			, when_completed
-			, when_recorded
-		FROM chore_completions
-		NATURAL JOIN chores
-        INNER JOIN chore_completion_hierarchy
-			ON chore_completions.chore_completion_id = chore_completion_hierarchy.parent_chore_completion_id
-		LEFT OUTER JOIN chore_sessions
-			ON chore_completion_hierarchy.chore_completion_id = chore_sessions.chore_completion_id
+        NATURAL JOIN chores
+        LEFT OUTER JOIN (SELECT chore_completion_id
+					, chore_session_id
+					, duration_minutes
+					, when_completed
+					, when_recorded
+				FROM chore_completions
+				NATURAL JOIN chore_sessions
+			UNION
+			SELECT parent_chore_completion_id
+					, chore_session_id
+					, duration_minutes
+					, when_completed
+					, when_recorded
+				FROM chore_completions
+				NATURAL JOIN chore_sessions
+				NATURAL JOIN chore_completion_hierarchy) AS chore_sessions_and_child_sessions
+			ON chore_completions.chore_completion_id = chore_sessions_and_child_sessions.chore_completion_id
 		WHERE chore_completions.chore_completion_id = chore_completion_id_to_show;
 END$$
 
