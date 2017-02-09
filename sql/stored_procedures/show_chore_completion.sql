ba37c0501f1d0d86fb531b72383af1e2048e80db
karldickman <karldickman@gmail.com>
2017-02-08 21:11:42 -0800

    modified:   show_chore_completion.sql     Show child chore instead of parent chore when dealing with a hierarchical     chore completion.

--- sql/stored_procedures/show_chore_completion.sql
+++ sql/stored_procedures/show_chore_completion.sql
@@ -6,14 +6,16 @@ USE chores$$
 CREATE PROCEDURE show_chore_completion(chore_completion_id_to_show INT)
 BEGIN
 	SELECT chore_completions.chore_completion_id
-			, chore
+			, COALESCE(child_chores.chore, chores.chore) AS chore
 			, chore_session_id
 			, duration_minutes
 			, when_completed
 			, when_recorded
 		FROM chore_completions
-        NATURAL JOIN chores
+        INNER JOIN chores
+			ON chore_completions.chore_id = chores.chore_id
         LEFT OUTER JOIN (SELECT chore_completion_id
+					, chore_id
 					, chore_session_id
 					, duration_minutes
 					, when_completed
@@ -22,6 +24,7 @@ BEGIN
 				NATURAL JOIN chore_sessions
 			UNION
 			SELECT parent_chore_completion_id
+					, chore_id
 					, chore_session_id
 					, duration_minutes
 					, when_completed
@@ -30,6 +33,8 @@ BEGIN
 				NATURAL JOIN chore_sessions
 				NATURAL JOIN chore_completion_hierarchy) AS chore_sessions_and_child_sessions
 			ON chore_completions.chore_completion_id = chore_sessions_and_child_sessions.chore_completion_id
+		LEFT OUTER JOIN chores AS child_chores
+			ON chore_sessions_and_child_sessions.chore_id = child_chores.chore_id
 		WHERE chore_completions.chore_completion_id = chore_completion_id_to_show;
 END$$
 
