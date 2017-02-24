478892a9927c155106eb05853d465a0db1edc81a
karldickman <karldickman@gmail.com>
2017-02-24 06:44:56 -0800

    Allow completion time to be provided when completing a chore without data.

--- sql/stored_procedures/complete_chore_without_data.sql
+++ sql/stored_procedures/complete_chore_without_data.sql
@@ -3,11 +3,11 @@ DROP PROCEDURE IF EXISTS complete_chore_without_data;
 
 DELIMITER $$
 USE chores$$
-CREATE PROCEDURE complete_chore_without_data (chore_name NVARCHAR(256), OUT found_chore_completion_id INT, OUT next_chore_completion_id INT)
+CREATE PROCEDURE complete_chore_without_data (chore_name NVARCHAR(256), when_completed DATETIME, OUT found_chore_completion_id INT, OUT next_chore_completion_id INT)
 BEGIN
 	SET found_chore_completion_id = NULL;
 	CALL get_chore_completion(chore_name, found_chore_completion_id);
-    CALL record_chore_completed(found_chore_completion_id, NULL, 3, next_chore_completion_id, TRUE);
+    CALL record_chore_completed(found_chore_completion_id, when_completed, 3, next_chore_completion_id, TRUE);
 END$$
 
 DELIMITER ;
