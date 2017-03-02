7df5d30850ae0fc0ca44e92bd4df83a583f4ff35
karldickman <karldickman@gmail.com>
2017-03-02 08:30:56 -0800

    Tweak chore completion times view to make it more useful.

--- sql/views/all_chore_completion_times.sql
+++ sql/views/all_chore_completion_times.sql
@@ -1,18 +1,18 @@
 DROP VIEW IF EXISTS all_chore_completion_times;
 CREATE VIEW all_chore_completion_times
 AS
-SELECT chore_id
+SELECT chore_completion_id
 		, when_completed
+        , 'chore_sessions' AS recorded_in
     FROM chore_sessions
     NATURAL JOIN chore_completions
-    NATURAL JOIN chores
     WHERE chore_completion_status_id IN (3, 4)
 UNION
-SELECT chore_id
+SELECT chore_completion_id
 		, when_completed
+        , 'chore_completion_times' AS recorded_in
 	FROM chore_completion_times
     NATURAL JOIN chore_completions
-    NATURAL JOIN chores
     WHERE chore_completion_status_id IN (3, 4)
 		AND chore_completion_id NOT IN (SELECT chore_completion_id
 				FROM chore_sessions);
