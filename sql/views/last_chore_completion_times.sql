99efade823af806e65a23bcbacb67fa06ca1130b
karldickman <karldickman@gmail.com>
2017-03-12 09:21:44 -0700

    Move "recursively measured" chore flag to its own table.

--- sql/views/last_chore_completion_times.sql
+++ sql/views/last_chore_completion_times.sql
@@ -3,4 +3,5 @@ CREATE VIEW last_chore_completion_times
 AS
 SELECT chore_id AS chore_id, MAX(when_completed) AS last_completed
 	FROM all_chore_completion_times
+    NATURAL JOIN chore_completions
     GROUP BY chore_id
