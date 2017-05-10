9a12f4187233cee9385beae313dbd2702ce835ba
karldickman <karldickman@gmail.com>
2017-05-10 07:41:14 -0700

    The wrong stored procedure code was stored in this file.

--- sql/stored_procedures/undo_chore_completion_status_change.sql
+++ sql/stored_procedures/undo_chore_completion_status_change.sql
@@ -1,14 +1,33 @@
 USE chores;
-DROP PROCEDURE IF EXISTS undo_chore_completion;
+DROP PROCEDURE IF EXISTS undo_chore_completion_status_change;
 
 DELIMITER $$
 USE chores$$
-CREATE PROCEDURE undo_chore_completion(chore_name NVARCHAR(256))
+
+CREATE PROCEDURE undo_chore_completion_status_change(chore_completion_to_undo_id INT)
 BEGIN
-	CALL get_chore_completion(chore_name, @found_chore_completion_id);
-	CALL delete_chore_completion(@found_chore_completion_id);
-	CALL get_chore_completion(chore_name, @found_chore_completion_id);
-    CALL undo_chore_completion_status_change(@found_chore_completion_id);
+	SET @last_status_change_date = NULL;
+    SELECT MAX(`to`) INTO @last_status_change_date
+		FROM chore_completion_status_history
+        WHERE chore_completion_id = chore_completion_to_undo_id;
+	SET @previous_chore_completion_status_id = NULL, @previous_status_since = NULL;
+    SELECT chore_completion_status_id INTO @previous_chore_completion_status_id
+		FROM chore_completion_status_history
+        WHERE chore_completion_id = chore_completion_to_undo_id
+			AND `to` = @last_status_change_date;
+    SELECT `from` INTO @previous_status_since
+		FROM chore_completion_status_history
+        WHERE chore_completion_id = chore_completion_to_undo_id
+			AND `to` = @last_status_change_date;
+	UPDATE chore_completions
+		SET chore_completion_status_id = @previous_chore_completion_status_id
+			, chore_completion_status_since = @previous_status_since
+		WHERE chore_completion_id = chore_completion_to_undo_id;
+	DELETE FROM chore_completion_status_history
+		WHERE chore_completion_id = chore_completion_to_undo_id
+			AND chore_completion_status_id = @previous_chore_completion_status_id
+            AND `from` = @previous_status_since
+            AND `to` = @last_status_change_date;
 END$$
 
 DELIMITER ;
