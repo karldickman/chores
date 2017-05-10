968712918653846b5d2dbc16d75be275b27d7aa8
karldickman <karldickman@gmail.com>
2017-05-10 07:42:41 -0700

    Avoid null chore completion times.

--- sql/views/all_chore_completion_times.sql
+++ sql/views/all_chore_completion_times.sql
@@ -2,17 +2,38 @@ DROP VIEW IF EXISTS all_chore_completion_times;
 CREATE VIEW all_chore_completion_times
 AS
 SELECT chore_completion_id
-		, when_completed
-        , 'chore_sessions' AS recorded_in
-    FROM chore_sessions
-    NATURAL JOIN chore_completions
-    WHERE chore_completion_status_id IN (3, 4)
-UNION
-SELECT chore_completion_id
-		, when_completed
-        , 'chore_completion_times' AS recorded_in
-	FROM chore_completion_times
-    NATURAL JOIN chore_completions
-    WHERE chore_completion_status_id IN (3, 4)
-		AND chore_completion_id NOT IN (SELECT chore_completion_id
-				FROM chore_sessions);
+		, MAX(when_completed) AS when_completed
+	FROM (SELECT chore_completion_id
+				, when_completed
+				, 'chore_sessions' AS recorded_in
+			FROM chore_sessions
+			NATURAL JOIN chore_completions
+			WHERE chore_completion_status_id IN (3, 4)
+		UNION
+		SELECT chore_completion_id
+				, when_completed
+				, 'chore_completion_times' AS recorded_in
+			FROM chore_completion_times
+			NATURAL JOIN chore_completions
+			WHERE chore_completion_status_id IN (3, 4)
+				AND chore_completion_id NOT IN (SELECT chore_completion_id
+						FROM chore_sessions)
+		UNION
+		SELECT parent_chore_completion_id
+				, when_completed
+				, 'chore_sessions' AS recorded_in
+			FROM chore_sessions
+			NATURAL JOIN chore_completions
+			NATURAL JOIN chore_completion_hierarchy
+			WHERE chore_completion_status_id IN (3, 4)
+		UNION
+		SELECT parent_chore_completion_id
+				, when_completed
+				, 'chore_completion_times' AS recorded_in
+			FROM chore_completion_times
+			NATURAL JOIN chore_completions
+			NATURAL JOIN chore_completion_hierarchy
+			WHERE chore_completion_status_id IN (3, 4)
+				AND chore_completion_id NOT IN (SELECT chore_completion_id
+						FROM chore_sessions)) AS combined
+	GROUP BY chore_completion_id
--- sql/views/chore_completions_when_completed.sql
+++ sql/views/chore_completions_when_completed.sql
@@ -1,10 +1,22 @@
 DROP VIEW IF EXISTS chore_completions_when_completed;
 CREATE VIEW chore_completions_when_completed
 AS
-SELECT chore_completion_id, when_completed
-	FROM chore_completion_durations
-UNION
-SELECT chore_completion_id, when_completed
-	FROM chore_completion_times
-    WHERE chore_completion_id NOT IN (SELECT chore_completion_id
-		FROM chore_sessions)
+SELECT chore_completion_id, MAX(when_completed) AS when_completed
+	FROM (SELECT chore_completion_id, when_completed
+			FROM chore_completion_durations
+		UNION
+		SELECT chore_completion_id, when_completed
+			FROM chore_completion_times
+			WHERE chore_completion_id NOT IN (SELECT chore_completion_id
+				FROM chore_sessions)
+		UNION
+		SELECT parent_chore_completion_id, when_completed
+			FROM chore_completion_durations
+			NATURAL JOIN chore_completion_hierarchy
+		UNION
+		SELECT parent_chore_completion_id, when_completed
+			FROM chore_completion_times
+            NATURAL JOIN chore_completion_hierarchy
+			WHERE chore_completion_id NOT IN (SELECT chore_completion_id
+				FROM chore_sessions)) AS combined_completions
+	GROUP BY chore_completion_id
