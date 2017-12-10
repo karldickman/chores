437122f673966fe6644bba5144d8b3d30170ed91
karldickman <karldickman@gmail.com>
2017-12-10 11:06:18 -0800

    Added duration column to show_chore_completion procedure.

--- sql/stored_procedures/show_chore_history.sql
+++ sql/stored_procedures/show_chore_history.sql
@@ -5,7 +5,12 @@ DELIMITER $$
 USE chores$$
 CREATE PROCEDURE show_chore_history(chore_name NVARCHAR(256))
 BEGIN
-	SELECT chore_completions.chore_completion_id, due_date, when_completed, chore_completion_status
+	SELECT chore_completions.chore_completion_id
+			, due_date
+            , chore_completion_durations.when_completed
+            , number_of_sessions
+            , TIME_FORMAT(SEC_TO_TIME(duration_minutes * 60), '%H:%i:%S') AS duration
+            , chore_completion_status
 		FROM chore_completions
 		NATURAL JOIN chore_completion_statuses
         NATURAL JOIN chores
@@ -13,6 +18,8 @@ BEGIN
 			ON chore_completions.chore_completion_id = chore_schedule.chore_completion_id
 		LEFT OUTER JOIN chore_completions_when_completed
 			ON chore_completions.chore_completion_id = chore_completions_when_completed.chore_completion_id
+		LEFT OUTER JOIN chore_completion_durations
+			ON chore_completions.chore_completion_id = chore_completion_durations.chore_completion_id
 		WHERE chore = chore_name
         ORDER BY due_date DESC, when_completed DESC;
 END$$
