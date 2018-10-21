2881dcfbe498ba3e65bc22d0e7431356b6f85bc5
karldickman <karldickman@gmail.com>
2018-10-21 16:07:53 -0700

    Split report on incomplete chores into two views for easier reuse.

---
+++ sql/views/incomplete_chores.sql
@@ -0,0 +1,29 @@
+DROP VIEW IF EXISTS incomplete_chores;
+CREATE VIEW incomplete_chores
+AS
+WITH chore_progress AS (SELECT chore_id
+		, chore_completion_id
+		, due_date
+		, last_completed
+		, completed_minutes
+		, remaining_minutes
+		, stdev_duration_minutes
+	FROM incomplete_chores_progress
+UNION ALL
+SELECT chore_id
+		, chore_completion_id
+		, due_date
+		, last_completed
+		, completed_minutes
+		, remaining_minutes
+		, stdev_duration_minutes
+	FROM never_measured_chores_progress)
+SELECT chore_id
+		, chore_completion_id
+		, due_date
+		, last_completed
+        , completed_minutes
+		, remaining_minutes
+		, stdev_duration_minutes
+        , remaining_minutes + (1.282 * stdev_duration_minutes) AS `90% CI UB`
+	FROM chore_progress
