d7131645e0987e02b50154befeda1dad98efb41c
karldickman <karldickman@gmail.com>
2018-12-23 10:33:11 -0800

    Add meals to chores completed and remaining procedure.

--- sql/stored_procedures/chores_completed_and_remaining.sql
+++ sql/stored_procedures/chores_completed_and_remaining.sql
@@ -6,7 +6,23 @@ DELIMITER $$
 CREATE PROCEDURE chores_completed_and_remaining(`from` DATE, `until` DATE)
 BEGIN
 	SET @until = DATE_ADD(DATE(`until`), INTERVAL 1 DAY);
-	WITH time_remaining_by_chore AS (SELECT incomplete_chores.chore_id
+	WITH meal_chores AS (SELECT chore_completion_id
+		FROM chore_completions
+		NATURAL JOIN chores
+		NATURAL JOIN chore_categories
+		NATURAL JOIN categories
+		WHERE category = 'meals'),
+	hierarchical_chore_schedule AS (SELECT chore_completion_id, due_date
+		FROM chore_schedule
+	UNION
+    SELECT chore_completions.chore_completion_id, due_date
+		FROM chore_completions
+        NATURAL JOIN chore_completion_hierarchy
+        INNER JOIN chore_schedule
+			ON parent_chore_completion_id = chore_schedule.chore_completion_id
+        WHERE chore_completions.chore_completion_id NOT IN (SELECT chore_completion_id
+				FROM chore_schedule)),
+    time_remaining_by_chore AS (SELECT incomplete_chores.chore_id
 			, chore_completion_id
 			, due_date
 			, FALSE AS is_completed
@@ -20,8 +36,10 @@ BEGIN
 		INNER JOIN chore_durations
 			ON incomplete_chores.chore_id = chore_durations.chore_id
 		WHERE due_date < @until
-			AND chore_completion_id NOT IN (SELECT chore_completion_id
-					FROM do_not_show_in_overdue_chores)
+			AND chore_completion_id NOT IN (SELECT parent_chore_completion_id
+					FROM chore_completion_hierarchy
+					NATURAL JOIN chore_completions
+					WHERE chore_completion_status_id = 1)
 	UNION
 	SELECT chore_completions.chore_id
 			, chore_completions.chore_completion_id
@@ -36,7 +54,7 @@ BEGIN
 		FROM chore_completions
         LEFT OUTER JOIN chore_completions_when_completed
 			ON chore_completions.chore_completion_id = chore_completions_when_completed.chore_completion_id
-		LEFT OUTER JOIN chore_schedule
+		LEFT OUTER JOIN hierarchical_chore_schedule AS chore_schedule
 			ON chore_completions.chore_completion_id = chore_schedule.chore_completion_id
 		LEFT OUTER JOIN chore_completion_durations
 			ON chore_completions.chore_completion_id = chore_completion_durations.chore_completion_id
@@ -48,10 +66,20 @@ BEGIN
 					FROM chore_completion_hierarchy
                     INNER JOIN chore_completions
 						ON parent_chore_completion_id = chore_completions.chore_completion_id
-					WHERE chore_completion_status_id = 4))
-	SELECT chore
+					WHERE chore_completion_status_id = 4)),
+	meal_summary AS (SELECT due_date
+			, MIN(is_completed) AS is_completed
+			, SUM(duration_minutes) AS duration_minutes
+			, SUM(completed_minutes) AS completed_minutes
+			, SUM(remaining_minutes) AS remaining_minutes
+			, SQRT(SUM(POWER(stdev_duration_minutes, 2))) AS stdev_duration_minutes
+		FROM time_remaining_by_chore
+        NATURAL JOIN meal_chores
+        GROUP BY due_date),
+	chores_and_meals AS (SELECT chore
 			, due_date
             , is_completed
+            , FALSE AS meal
 			, frequency IS NOT NULL AND frequency <= 7 AND frequency_unit_id = 1 /*Days*/ AS weekly
 			, duration_minutes
 			, completed_minutes
@@ -62,7 +90,40 @@ BEGIN
 		NATURAL JOIN chores
 		LEFT OUTER JOIN chore_frequencies
 			ON time_remaining_by_chore.chore_id = chore_frequencies.chore_id
-		ORDER BY weekly DESC, is_completed, remaining_minutes DESC, duration_minutes;
+		WHERE chore_completion_id NOT IN (SELECT chore_completion_id
+			FROM meal_chores)
+	UNION
+    SELECT CONCAT('meals ', DATE_FORMAT(due_date, '%m-%d')) AS chore
+			, due_date
+            , is_completed
+            , TRUE AS meal
+			, TRUE AS weekly
+			, duration_minutes
+			, completed_minutes
+			, remaining_minutes
+			, stdev_duration_minutes
+			, remaining_minutes + (1.282 * stdev_duration_minutes) AS `90% CI UB`
+		FROM meal_summary)
+	SELECT chore
+			, due_date
+            , is_completed
+            , weekly
+            , duration_minutes
+            , completed_minutes
+            , remaining_minutes
+            , stdev_duration_minutes
+            , `90% CI UB`
+		FROM chores_and_meals
+		ORDER BY meal DESC
+			, weekly DESC
+			, CASE
+				WHEN meal
+					THEN due_date
+				ELSE 0
+                END
+            , is_completed
+            , remaining_minutes DESC
+            , duration_minutes;
 END$$
 
 DELIMITER ;
