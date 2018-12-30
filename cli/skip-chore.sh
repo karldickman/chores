07c79db783eba0d613fd95dbc56d67a73d694c32
karldickman <karldickman@gmail.com>
2018-12-29 19:01:33 -0800

    Created bash wrapper around skip_chore stored procedure.

---
+++ cli/skip-chore.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+usage="$(basename "$0") CHORE [OPTIONS]
+
+Record a chore completion.
+Arguments:
+    CHORE          The name of the chore completed.
+Options:
+    -h, --help     Show this help text and exit.
+    --preview      Show the SQL command to be executed.
+    -v, --verbose  Show SQL commands as they are executed."
+
+# Process options
+i=0
+execute=1
+verbose=0
+for arg in "$@"
+do
+	if [[ $arg != -* ]]
+	then
+		arguments[$i]=$arg
+		((i++))
+	fi
+	if [[ $arg == "-h" ]] || [[ $arg == "--help" ]]
+	then
+		echo "$usage"
+		exit
+	fi
+	if [[ $arg == "--preview" ]]
+	then
+		execute=0
+		verbose=1
+	fi
+	if [[ $arg == "-v" ]] || [[ $arg == "--verbose" ]]
+	then
+		verbose=1
+	fi
+done
+
+# Process arguments
+if [[ ${#arguments[@]} -lt 1 ]]
+then
+	echo "Missing required argument CHORE."
+	echo "$usage"
+	exit 1
+fi
+chore=${arguments[0]//\'/\\\'}
+
+# Invoke SQL
+sql="CALL skip_chore('$chore', @c, @n)"
+
+
+if [[ $verbose -eq 1 ]]
+then
+	echo "$sql"
+fi
+if [[ $execute -eq 1 ]]
+then
+	mysql chores -u chores -pM2TEncult7v3TrC90SUs -e "$sql"
+fi
