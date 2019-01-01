8f206b4928465e3bfdcdf6e1f4fd9a12dc9e1658
karldickman <karldickman@gmail.com>
2018-12-31 16:45:41 -0800

    Merge minutes and seconds duration parameters.

--- sql/stored_procedures/complete_chore.sql
+++ sql/stored_procedures/complete_chore.sql
@@ -2,20 +2,19 @@ USE chores;
 DROP PROCEDURE IF EXISTS complete_chore;
 
 DELIMITER $$
-USE chores$$
+
 CREATE PROCEDURE complete_chore(
 	chore_name NVARCHAR(256),
     when_completed DATETIME,
-    minutes FLOAT,
-    seconds FLOAT,
+    duration_minutes FLOAT,
     OUT found_chore_completion_id INT,
     OUT next_chore_completion_id INT)
 BEGIN
 	SET found_chore_completion_id = NULL;
     SET @new_chore_session_id = NULL;
-	IF when_completed IS NOT NULL AND minutes IS NOT NULL and seconds IS NOT NULL
+	IF when_completed IS NOT NULL AND duration_minutes IS NOT NULL
     THEN
-		CALL complete_chore_session(chore_name, when_completed, minutes, seconds, found_chore_completion_id, @new_chore_session_id);
+		CALL complete_chore_session(chore_name, when_completed, duration_minutes, found_chore_completion_id, @new_chore_session_id);
 	ELSE
 		CALL get_chore_completion(chore_name, found_chore_completion_id);
 	END IF;
