babc2cc17fce7c212dc59ef4054d5dc62771b95a
karldickman <karldickman@gmail.com>
2018-12-31 22:06:26 -0800

    Added more commands to command list script.

--- cli/list-commands.sh
+++ cli/list-commands.sh
@@ -3,7 +3,11 @@ echo "chores-complete
     Record that a chore is completed.
 chores-get-completion-id
     Get the database identifier of the active completion of a chore.
+chores-put-away-dishes
+	Record how long it took to put away the dishes.
 chores-session
     Record that some time was spent working on a chore.
 chores-skip
-    Skip doing a chore."
+    Skip doing a chore.
+chores-unknown-duration
+	Record that was completed but it is not known how long it took."
