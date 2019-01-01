16daf75ddc8f7790cf8991cd0228822bd6acab18
karldickman <karldickman@gmail.com>
2018-12-31 18:26:39 -0800

    Added command to list all commands in the chores suite.

---
+++ cli/list-commands.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "chores-complete
+    Record that a chore is completed.
+chores-get-completion-id
+    Get the database identifier of the active completion of a chore.
+chores-session
+    Record that some time was spent working on a chore.
+chores-skip
+    Skip doing a chore."
