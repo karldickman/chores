346ace31b99617d4db0a80937d1eda0b6aef761d
karldickman <karldickman@gmail.com>
2018-12-31 19:57:57 -0800

    Merge minutes and seconds into single parameter in put away dishes procedure.

--- sql/stored_procedures/complete_unscheduled_chore.sql
+++ sql/stored_procedures/complete_unscheduled_chore.sql
@@ -6,13 +6,12 @@ DELIMITER $$
 CREATE PROCEDURE complete_unscheduled_chore(
 	chore_name NVARCHAR(256),
     when_completed DATETIME,
-    minutes FLOAT,
-    seconds FLOAT,
+    duration_minutes FLOAT,
     OUT new_chore_completion_id INT,
     OUT next_chore_completion_id INT)
 BEGIN
 	CALL create_chore_completion(chore_name, new_chore_completion_id);
-    CALL record_chore_session(new_chore_completion_id, when_completed, minutes + seconds / 60.0, @new_chore_session_id);
+    CALL record_chore_session(new_chore_completion_id, when_completed, duration_minutes, @new_chore_session_id);
     CALL record_chore_completed(new_chore_completion_id, NULL, 4, FALSE);
     IF next_chore_completion_id = TRUE
     THEN
