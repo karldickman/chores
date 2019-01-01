c6c9d50773b644d5499fe752f9da5678c19ca6c5
karldickman <karldickman@gmail.com>
2018-12-31 18:11:44 -0800

    Remove redundant USE statement.

--- sql/stored_procedures/get_chore_completion.sql
+++ sql/stored_procedures/get_chore_completion.sql
@@ -2,7 +2,7 @@ USE chores;
 DROP PROCEDURE IF EXISTS get_chore_completion;
 
 DELIMITER $$
-USE chores$$
+
 CREATE PROCEDURE get_chore_completion (chore_name NVARCHAR(256), OUT found_chore_completion_id INT)
 this_procedure:BEGIN
 	SET @earliest_due_date = NULL;
