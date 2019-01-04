03aebf5a2f08ae25d0ed1a7aefa48bfee9eed679
karldickman <karldickman@gmail.com>
2019-01-03 18:34:46 -0800

    Added procedure for seeing next due dates of incomplete chores.

--- sql/views/chore_completion_next_due_dates.sql
+++ sql/views/chore_completion_next_due_dates.sql
@@ -1,7 +1,8 @@
 DROP VIEW IF EXISTS chore_completion_next_due_dates;
 CREATE VIEW chore_completion_next_due_dates
 AS
-SELECT chore_completion_id
+SELECT chore_completions.chore_completion_id
+		, chore_completions.chore_completion_status_id
 		, schedule_from_date
         , chore_completions_schedule_from_dates.schedule_from_id
         , frequency
@@ -13,10 +14,11 @@ SELECT chore_completion_id
 			WHEN time_unit = 'month'
 				THEN DATE_ADD(schedule_from_date, INTERVAL frequency MONTH)
 			END AS next_due_date
-	FROM chore_completions_schedule_from_dates
-    NATURAL JOIN chore_completions
+	FROM chore_completions
+    INNER JOIN chore_completions_schedule_from_dates
+		ON chore_completions.chore_completion_id = chore_completions_schedule_from_dates.chore_completion_id
     INNER JOIN chore_frequencies
 		ON chore_completions.chore_id = chore_frequencies.chore_id
     INNER JOIN time_units
 		ON frequency_unit_id = time_units.time_unit_id
-    ORDER BY chore_completion_id
+    ORDER BY chore_completions.chore_completion_id
--- sql/views/chore_completions_schedule_from_dates.sql
+++ sql/views/chore_completions_schedule_from_dates.sql
@@ -2,6 +2,7 @@ DROP VIEW IF EXISTS chore_completions_schedule_from_dates;
 CREATE VIEW chore_completions_schedule_from_dates
 AS
 SELECT chore_completion_id
+		, chore_completion_status_id
 		, when_completed AS schedule_from_date
         , schedule_from_id
 	FROM chore_completions_schedule_from
@@ -9,8 +10,17 @@ SELECT chore_completion_id
     WHERE schedule_from_id = 1 # Completion time
 UNION
 SELECT chore_completion_id
+		, chore_completion_status_id
 		, due_date
         , schedule_from_id
 	FROM chore_completions_schedule_from
     NATURAL JOIN chore_schedule
     WHERE schedule_from_id = 2 # Due date
+UNION
+SELECT chore_completion_id
+		, chore_completion_status_id
+		, due_date
+        , 2 AS schedule_from_id # Due date
+	FROM chore_completions
+    NATURAL JOIN chore_schedule
+    WHERE chore_completion_status_id = 1 # Scheduled
