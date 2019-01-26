919b3728e3013bd6ba3cea1eb2f6522e293ed7ff
karldickman <karldickman@gmail.com>
2019-01-26 07:20:15 -0800

    Add missing permissions.

--- sql/permissions.sql
+++ sql/permissions.sql
@@ -1,3 +1,4 @@
 USE chores;
 GRANT EXECUTE ON PROCEDURE chores.chore_duration_today TO 'chores'@'localhost';
+GRANT EXECUTE ON PROCEDURE chores.get_chore_completion TO 'chores'@'localhost';
 GRANT EXECUTE ON PROCEDURE chores.show_meal_chores TO 'chores'@'localhost';
