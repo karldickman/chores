7dec2b0e59646b845eae5453889dc23094ae0bb8
karldickman <karldickman@gmail.com>
2019-01-26 07:21:31 -0800

    Added postpone chore stored procedure.

--- sql/permissions.sql
+++ sql/permissions.sql
@@ -1,4 +1,5 @@
 USE chores;
 GRANT EXECUTE ON PROCEDURE chores.chore_duration_today TO 'chores'@'localhost';
 GRANT EXECUTE ON PROCEDURE chores.get_chore_completion TO 'chores'@'localhost';
+GRANT EXECUTE ON PROCEDURE chores.postpone_chore TO 'chores'@'localhost';
 GRANT EXECUTE ON PROCEDURE chores.show_meal_chores TO 'chores'@'localhost';
---
+++ sql/stored_procedures/postpone_chore.sql
@@ -0,0 +1,14 @@
+USE chores;
+DROP PROCEDURE IF EXISTS postpone_chore;
+
+DELIMITER $$
+
+CREATE PROCEDURE postpone_chore(chore_name NVARCHAR(256), days INT, OUT found_chore_completion_id INT)
+BEGIN
+	CALL get_chore_completion(chore_name, found_chore_completion_id);
+    UPDATE chore_schedule
+		SET due_date = DATE_ADD(due_date, INTERVAL days DAY)
+		WHERE chore_completion_id = found_chore_completion_id;
+END$$
+
+DELIMITER ;
