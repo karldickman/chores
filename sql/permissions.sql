b8f1a31f1fbd6b7b846d9b3db6b00a64aabe80e0
karldickman <karldickman@gmail.com>
2019-02-01 19:14:29 -0800

    Chore dot qualification redundant with using statement.

--- sql/permissions.sql
+++ sql/permissions.sql
@@ -1,6 +1,6 @@
 USE chores;
-GRANT EXECUTE ON PROCEDURE chores.chores_completed_and_remaining TO 'chores'@'localhost';
-GRANT EXECUTE ON PROCEDURE chores.chore_duration_today TO 'chores'@'localhost';
-GRANT EXECUTE ON PROCEDURE chores.get_chore_completion TO 'chores'@'localhost';
-GRANT EXECUTE ON PROCEDURE chores.postpone_chore TO 'chores'@'localhost';
-GRANT EXECUTE ON PROCEDURE chores.show_meal_chores TO 'chores'@'localhost';
+GRANT EXECUTE ON PROCEDURE chores_completed_and_remaining TO 'chores'@'localhost';
+GRANT EXECUTE ON PROCEDURE chore_duration_today TO 'chores'@'localhost';
+GRANT EXECUTE ON PROCEDURE get_chore_completion TO 'chores'@'localhost';
+GRANT EXECUTE ON PROCEDURE postpone_chore TO 'chores'@'localhost';
+GRANT EXECUTE ON PROCEDURE show_meal_chores TO 'chores'@'localhost';
