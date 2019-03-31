e344a1079dc3d0b2d8bf04a45e923fad51b01219
karldickman <karldickman@gmail.com>
2019-03-31 10:28:13 -0700

    Formatting

--- sql/views/chore_durations.sql
+++ sql/views/chore_durations.sql
@@ -8,7 +8,10 @@ SELECT chore_id
 		, COUNT(chore_completion_id) AS times_completed
         , AVG(1.0 * number_of_sessions) AS avg_number_of_sessions
         , AVG(duration_minutes) AS avg_duration_minutes
-        , CASE WHEN COUNT(chore_completion_id) > 1 THEN STD(duration_minutes) END AS stdev_duration_minutes
+        , CASE
+			WHEN COUNT(chore_completion_id) > 1
+				THEN STD(duration_minutes)
+			END AS stdev_duration_minutes
 	FROM hierarchical_chore_completion_durations
     NATURAL JOIN chore_completions
     WHERE chore_completion_status_id = 4
