dfce5a97af9e4b167ec1528ca8f6cfb432d64bf2
karldickman <karldickman@gmail.com>
2019-08-04 09:53:06 -0700

    Grant permissions to execute record chore session procedure.

--- sql/permissions.sql
+++ sql/permissions.sql
@@ -4,6 +4,7 @@ GRANT EXECUTE ON PROCEDURE chore_duration_today TO 'chores'@'localhost';
 GRANT EXECUTE ON PROCEDURE delete_chore_completion TO 'chores'@'localhost';
 GRANT EXECUTE ON PROCEDURE get_chore_completion TO 'chores'@'localhost';
 GRANT EXECUTE ON PROCEDURE postpone_chore TO 'chores'@'localhost';
+GRANT EXECUTE ON PROCEDURE record_chore_session TO 'chores'@'localhost';
 GRANT EXECUTE ON PROCEDURE show_chore_completion TO 'chores'@'localhost';
 GRANT EXECUTE ON PROCEDURE show_chore_history TO 'chores'@'localhost';
 GRANT EXECUTE ON PROCEDURE show_meal_chores TO 'chores'@'localhost';
