46dfc100eff1f675b78b758af1cd54435315be02
karldickman <karldickman@gmail.com>
2019-12-11 09:07:26 -0800

    Modify how hierarchical chore completion times are reported.

--- sql/views/chore_completions_when_completed.sql
+++ sql/views/chore_completions_when_completed.sql
@@ -4,21 +4,20 @@ DROP VIEW IF EXISTS chore_completions_when_completed;
 
 CREATE VIEW chore_completions_when_completed
 AS
-WITH combined_completions AS (SELECT chore_completion_id
-        , MAX(when_completed) AS when_completed
-	FROM hierarchical_chore_sessions
-    GROUP BY chore_completion_id
-UNION
-SELECT chore_completion_id, when_completed
+WITH flatten_chore_completion_times_hierarchy AS (SELECT chore_completion_id, when_completed
 	FROM chore_completion_times
-	WHERE chore_completion_id NOT IN (SELECT chore_completion_id
-		FROM chore_sessions)
 UNION
 SELECT parent_chore_completion_id, when_completed
 	FROM chore_completion_times
-	NATURAL JOIN chore_completion_hierarchy
-	WHERE chore_completion_id NOT IN (SELECT chore_completion_id
-		FROM chore_sessions))
+	NATURAL JOIN chore_completion_hierarchy),
+hierarchical_chore_completion_times AS (SELECT chore_completion_id, MAX(when_completed) AS when_completed
+	FROM flatten_chore_completion_times_hierarchy
+    GROUP BY chore_completion_id)
+SELECT chore_completion_id, when_completed
+	FROM hierarchical_chore_completion_times
+UNION
 SELECT chore_completion_id, MAX(when_completed) AS when_completed
-	FROM combined_completions
-	GROUP BY chore_completion_id
+	FROM hierarchical_chore_sessions
+    WHERE chore_completion_id NOT IN (SELECT chore_completion_id
+			FROM hierarchical_chore_completion_times)
+    GROUP BY chore_completion_id
