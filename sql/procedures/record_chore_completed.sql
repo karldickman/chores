0ce57d4cac13ff80a11989b75a87c97801954abc
karldickman <karldickman@gmail.com>
2019-12-11 09:06:29 -0800

    Signal error when chore completion time has already been recoreded.

--- sql/procedures/record_chore_completed.sql
+++ sql/procedures/record_chore_completed.sql
@@ -6,10 +6,13 @@ DELIMITER $$
 CREATE PROCEDURE record_chore_completed (completed_chore_completion_id INT, when_completed DATETIME, new_chore_completion_status_id INT, update_history BIT)
 BEGIN
 	IF when_completed IS NOT NULL
-		AND NOT EXISTS(SELECT *
-			FROM chore_sessions
-			WHERE chore_sessions.chore_completion_id = completed_chore_completion_id)
+		AND new_chore_completion_status_id = 3 # No data
 	THEN
+		IF EXISTS(SELECT * FROM chore_completion_times WHERE chore_completion_id = completed_chore_completion_id)
+        THEN
+			SET @error_message = CONCAT('Chore completion id ', completed_chore_completion_id, ' already has a completion time.');
+			SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = @error_message;
+        END IF;
 		INSERT INTO chore_completion_times
 			(chore_completion_id, when_completed)
 			VALUES
