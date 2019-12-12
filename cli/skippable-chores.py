77e66ec91babefc7952eb1d1c15ff0c0a3ea70ba
karldickman <karldickman@gmail.com>
2019-12-11 19:15:54 -0800

    Command line interface to list skippable chores.

---
+++ cli/skippable-chores.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from argparse import ArgumentParser
+from subprocess import call
+from sys import argv
+
+def main(arguments):
+    sql = "SELECT * FROM skippable_chores"
+    command = ["chore-database", sql]
+    command.extend(argv[1:])
+    call(command)
+
+def parse_arguments():
+    parser = ArgumentParser(description="Show overdue chores that should be skipped.")
+    parser.add_argument("--preview", action="store_true", help="Show the SQL command but do not execute it.")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Show SQL commands as they are executed.")
+    return parser.parse_args()
+
+if __name__ == "__main__":
+    arguments = parse_arguments()
+    main(arguments)
+
--- sql/permissions.sql
+++ sql/permissions.sql
@@ -1,4 +1,5 @@
 USE chores;
+GRANT SELECT ON skippable_chores TO 'chores'@'localhost';
 GRANT EXECUTE ON PROCEDURE chores_completed_and_remaining TO 'chores'@'localhost';
 GRANT EXECUTE ON PROCEDURE chore_duration_today TO 'chores'@'localhost';
 GRANT EXECUTE ON PROCEDURE delete_chore_completion TO 'chores'@'localhost';
@@ -8,3 +9,4 @@ GRANT EXECUTE ON PROCEDURE record_chore_session TO 'chores'@'localhost';
 GRANT EXECUTE ON PROCEDURE show_chore_completion TO 'chores'@'localhost';
 GRANT EXECUTE ON PROCEDURE show_chore_history TO 'chores'@'localhost';
 GRANT EXECUTE ON PROCEDURE show_meal_chores TO 'chores'@'localhost';
+
