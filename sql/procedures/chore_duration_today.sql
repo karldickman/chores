b86b09bc54a6434703d6d98a1a3d2fc88f6b96c7
karldickman <karldickman@gmail.com>
2019-12-21 15:05:58 -0800

    chore duration today procedure should show all chores.

--- sql/procedures/chore_duration_today.sql
+++ sql/procedures/chore_duration_today.sql
@@ -6,22 +6,10 @@ DELIMITER $$
 
 CREATE PROCEDURE chore_duration_today()
 BEGIN
-    SET @day = DATE(NOW());
-    WITH irrelevant_chores AS (SELECT chore_id
-        FROM chore_categories
-        NATURAL JOIN categories
-        WHERE category = 'meals'
-    UNION
-    SELECT chore_id
-        FROM chores
-        WHERE chore IN ('close budget period', 'recalculate contribution split'))
+    SET @`day` = DATE(NOW());
     SELECT IFNULL(SUM(duration_minutes), 0) AS duration
         FROM chore_sessions
-        NATURAL JOIN chore_completions
-        NATURAL JOIN chores
-        WHERE when_completed BETWEEN @day AND DATE_ADD(@day, INTERVAL 1 DAY)
-            AND chore_id NOT IN (SELECT chore_id
-                    FROM irrelevant_chores);
+        WHERE when_completed BETWEEN @`day` AND DATE_ADD(@`day`, INTERVAL 1 DAY);
 END$$
 
 DELIMITER ;
