90b4d67360b30e1804a9975c5faaef87896927e7
karldickman <karldickman@gmail.com>
2020-04-15 19:00:14 -0700

    Add view of chore standard errors.

---
+++ sql/views/chore_stderrs.sql
@@ -0,0 +1,38 @@
+DROP VIEW IF EXISTS chore_stderrs;
+
+CREATE VIEW chore_stderrs
+AS
+WITH stderr AS (SELECT chore
+        , times_completed
+        , avg_number_of_sessions
+        , avg_duration_minutes
+        , COALESCE(stdev_duration_minutes, 3.402823466E+38) AS stdev_duration_minutes
+        , COALESCE(stdev_duration_minutes, 3.402823466E+38) / SQRT(times_completed) AS stderr_duration_minutes
+    FROM chore_durations
+    NATURAL JOIN chores
+UNION
+SELECT chore
+        , 0 AS times_completed
+        , NULL AS avg_number_of_sessions
+        , NULL AS avg_duration_minutes
+        , 3.402823466E+38 AS stdev_duration_minutes
+        , 3.402823466E+38 AS stderr_duration_minutes
+    FROM chores
+    WHERE chore_id NOT IN (SELECT chore_id
+            FROM chore_durations)),
+confidence_intervals AS (SELECT chore
+        , times_completed
+        , avg_number_of_sessions
+        , avg_duration_minutes
+        , stdev_duration_minutes
+        , stderr_duration_minutes
+        , 1.96 * stderr_duration_minutes AS `95% CI`
+    FROM stderr)
+SELECT chore
+        , times_completed
+        , avg_number_of_sessions
+        , avg_duration_minutes
+        , stdev_duration_minutes
+        , stderr_duration_minutes
+        , `95% CI`
+    FROM confidence_intervals
