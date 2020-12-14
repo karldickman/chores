4cbbc7920f8b7651140fda9e2c650220505c7019
karldickman <karldickman@gmail.com>
2020-12-13 16:44:17 -0800

    Include chores scheduled by a fixed due date in chore periods views.

--- sql/views/chore_completions_per_day.sql
+++ sql/views/chore_completions_per_day.sql
@@ -2,34 +2,15 @@ USE chores;
 # DROP VIEW chore_completions_per_day;
 CREATE OR REPLACE VIEW chore_completions_per_day
 AS
-WITH chore_completions_per_year AS (SELECT chore_id
-        , COUNT(*) AS completions_per_year
-        , MIN(since) AS since
-    FROM chore_due_dates
-    GROUP BY chore_id)
-SELECT chore_id
+SELECT `source`
+        , chore_id
         , chore
         , aggregate_by_id
         , period
         , period_unit_id
         , period_days
-        , completions_per_day
+        , completions_per_day / period_days AS completions_per_day
         , period_since
         , schedule_from_id
         , schedule_from_id_since
-    FROM chore_completions_per_day_from_period
-UNION
-SELECT chore_id
-        , chore
-        , aggregate_by_id
-        , 365 / completions_per_year AS period
-        , 1 AS period_unit_id
-        , 365 / completions_per_year AS period_days
-        , completions_per_year / 365 AS completions_per_day
-        , since AS period_since
-        , 3 AS schedule_from_id
-        , since AS schedule_from_id_since
-    FROM chore_completions_per_year
-    JOIN chores USING (chore_id)
-    WHERE chore_id NOT IN (SELECT chore_id
-            FROM chore_completions_per_day_from_period);
+    FROM chore_periods_days;
--- sql/views/chore_periods_days.sql
+++ sql/views/chore_periods_days.sql
@@ -1,7 +1,15 @@
 USE chores;
-CREATE VIEW chore_periods_days
+CREATE OR REPLACE VIEW chore_periods_days
 AS
-SELECT chore_id
+WITH chore_completions_per_year AS (SELECT chore_id
+        , COUNT(*) AS completions_per_year
+        , MIN(since) AS since
+    FROM chore_due_dates
+    GROUP BY chore_id)
+SELECT 'chore_frequencies' AS `source`
+        , chore_id
+        , chore
+        , aggregate_by_id
         , frequency AS period
         , frequency_unit_id AS period_unit_id
         , frequency * CASE
@@ -9,9 +17,27 @@ SELECT chore_id
                 THEN 365 / 12
             ELSE 1
             END AS period_days
+        , completions_per_day
         , frequency_since AS period_since
         , schedule_from_id
         , schedule_from_id_since
     FROM chore_frequencies
+    JOIN chores USING (chore_id)
     JOIN time_units
-        ON frequency_unit_id = time_unit_id;
+        ON frequency_unit_id = time_unit_id
+UNION
+SELECT 'chore_due_dates' AS `source`
+        , chore_id
+        , chore
+        , aggregate_by_id
+        , 365 / completions_per_year AS period
+        , 1 AS period_unit_id
+        , 365 / completions_per_year AS period_days
+        , completions_per_year / 365 AS completions_per_day
+        , since AS period_since
+        , 3 AS schedule_from_id
+        , since AS schedule_from_id_since
+    FROM chore_completions_per_year
+    JOIN chores USING (chore_id)
+    WHERE chore_id NOT IN (SELECT chore_id
+            FROM chore_frequencies);     
