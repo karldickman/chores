a5f9abdf90cc91d4e9e09c95ba151a3cefafaf65
karldickman <karldickman@gmail.com>
2020-12-25 18:37:00 -0800

    Modify chore creation procedure to match new schedule from schema.

--- sql/procedures/create_chore.sql
+++ sql/procedures/create_chore.sql
@@ -14,11 +14,15 @@ BEGIN
     IF frequency_days IS NOT NULL
     THEN
         SET @day_unit_id = 1;
-        SET @schedule_from_completion_time = 1;
         INSERT INTO chore_frequencies
-            (chore_id, frequency, frequency_unit_id, schedule_from_id)
+            (chore_id, frequency, frequency_unit_id)
             VALUES
-            (new_chore_id, frequency_days, @day_unit_id, @schedule_from_completion_time);
+            (new_chore_id, frequency_days, @day_unit_id);
+        SET @schedule_from_completion_time = 1;
+        INSERT INTO chore_schedule_from
+            (chore_id, schedule_from_id)
+            VALUES
+            (new_chore_id, @schedule_from_completion_time);
     END IF;
 END$$
 
