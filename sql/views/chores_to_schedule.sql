3ffe5010c5fc30dace36ba7b4d2e6ccecf344550
karldickman <karldickman@gmail.com>
2021-01-24 08:01:40 -0800

    Fix chores to schedule view bug.

--- sql/views/chores_to_schedule.sql
+++ sql/views/chores_to_schedule.sql
@@ -1,38 +1,25 @@
 USE chores;
 
-DROP VIEW IF EXISTS chores_to_schedule;
-
-CREATE VIEW chores_to_schedule
+CREATE OR REPLACE VIEW chores_to_schedule
 AS
 WITH last_due_dates AS (SELECT chore_id, MAX(due_date) AS last_due_date
     FROM chore_schedule
-    NATURAL JOIN chore_completions
-    NATURAL JOIN chore_schedule_in_advance
-    NATURAL JOIN schedule_from
-    WHERE schedule_from = 'due date'
+    JOIN chore_completions USING (chore_completion_id)
+    JOIN chore_schedule_in_advance USING (chore_id)
+    JOIN chore_schedule_from USING (chore_id)
+    WHERE schedule_from_id = 2 # 'due date'
     GROUP BY chore_id),
 scheduling_parameters AS (SELECT chore_id
-        , CASE
-            WHEN time_unit = 'day'
-                THEN DATE_ADD(last_due_date, INTERVAL frequency - days_in_advance DAY)
-            WHEN time_unit = 'month'
-                THEN DATE_ADD(DATE_ADD(last_due_date, INTERVAL frequency MONTH), INTERVAL -days_in_advance DAY)
-            END AS schedule_on
-        , CASE
-            WHEN time_unit = 'day'
-                THEN DATE_ADD(last_due_date, INTERVAL frequency DAY)
-            WHEN time_unit = 'month'
-                THEN DATE_ADD(last_due_date, INTERVAL frequency MONTH)
-            END AS next_due_date
+        , DATE_ADD(last_due_date, INTERVAL period_days - days_in_advance DAY) AS schedule_on
+        , DATE_ADD(last_due_date, INTERVAL period_days DAY) AS next_due_date
     FROM last_due_dates
-    NATURAL JOIN chore_schedule_in_advance
-    NATURAL JOIN chore_frequencies
-    NATURAL JOIN time_units) 
+    JOIN chore_schedule_in_advance USING (chore_id)
+    JOIN chore_periods_days USING (chore_id)) 
 SELECT chore_id, schedule_on, next_due_date
     FROM scheduling_parameters
     WHERE schedule_on <= DATE(CURRENT_TIMESTAMP)
         AND NOT EXISTS(SELECT *
                 FROM chore_schedule
-                NATURAL JOIN chore_completions
+                JOIN chore_completions USING (chore_completion_id)
                 WHERE chore_completions.chore_id = scheduling_parameters.chore_id
-                    AND due_date = next_due_date)
+                    AND due_date = next_due_date);
