25d3501d966aba5f1c45ad120582506b99136e50
karldickman <karldickman@gmail.com>
2021-01-29 09:42:39 -0800

    Wrap long lines.

--- R/progress.R
+++ R/progress.R
@@ -80,7 +80,11 @@ chores.completed.and.remaining.chart <- function (data) {
     layout(
       barmode = "stack",
       yaxis = list(title = "Duration (minutes)"),
-      yaxis2 = list(overlaying = "y", side = "right", title = "Cumulative duration (hours)", rangemode = "tozero"),
+      yaxis2 = list(
+        overlaying = "y",
+        side = "right",
+        title = "Cumulative duration (hours)",
+        rangemode = "tozero"),
       legend = list(
         orientation = "h",
         traceorder = "normal",
