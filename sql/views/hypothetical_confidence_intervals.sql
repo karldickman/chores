ec31ae15ac06ecc2daf548ec1ccb1656c90e5a5d
karldickman <karldickman@gmail.com>
2021-05-15 06:45:41 -0700

    Fix off-by-one in degrees of freedom calculation.

--- sql/views/hypothetical_confidence_intervals.sql
+++ sql/views/hypothetical_confidence_intervals.sql
@@ -20,7 +20,7 @@ SELECT chore_id
         , `one tail 95% CI UB`
         , students_t_critical_values.degrees_of_freedom AS hypothetical_degrees_of_freedom
         , students_t_critical_values.critical_value AS hypothetical_critical_value
-        , EXP(log_normal_confidence_bound(mean_log_duration_minutes, sd_log_duration_minutes, students_t_critical_values.degrees_of_freedom, students_t_critical_values.critical_value)) AS `hypothetical 95% CI UB`
+        , EXP(log_normal_confidence_bound(mean_log_duration_minutes, sd_log_duration_minutes, students_t_critical_values.degrees_of_freedom + 1, students_t_critical_values.critical_value)) AS `hypothetical 95% CI UB`
     FROM chore_duration_confidence_intervals
     LEFT JOIN students_t_critical_values
         ON one_tail_confidence = 0.05
