d6f40de978de3b8a35b7c6ea2d55199ec35c09a1
Karl Dickman <karldickman@gmail.com>
2022-11-05 06:58:58 -0700

    Add log normal confidence bound function.

--- R/log_normal.R
+++ R/log_normal.R
@@ -19,3 +19,22 @@ scale.log.normal.mean <- function (mean, factor) {
 rvlnorm <- function (n = 1, mean = 0, sd = 1, var = NULL, precision) {
   exp(sims(rvnorm(n, mean, sd, var, precision)))
 }
+
+log.normal.confidence.bound <- function (sample.mean, sample.sd, sample.size, significance, tail = "both") {
+  sample.variance <- sample.sd ^ 2
+  augend <- sample.mean + sample.variance / 2
+  if (tail == "both") {
+    significance <- significance / 2
+  }
+  critical.value <- qt(p = significance, df = sample.size - 1, lower.tail = FALSE)
+  addend <- critical.value * sqrt(sample.variance / sample.size + sample.variance ^ 2 / (2 * (sample.size - 1)))
+  lower <- augend - addend
+  upper <- augend + addend
+  if (tail == "lower") {
+    return (lower)
+  }
+  if (tail == "upper") {
+    return (upper)
+  }
+  c(lower, upper)
+}
