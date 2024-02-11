3a51e668b1029f84ca28d0a3d67b524ae5eec418
Karl Dickman <karldickman@gmail.com>
2024-02-11 08:40:19 -0800

    Create chore aliases and installation utility.

---
+++ cli/chore-aliases.sh
@@ -0,0 +1,7 @@
+alias complete=chore-complete
+alias meals=chore-meals
+alias postpone=chore-postpone
+alias session=chore-session
+alias skip=chore-skip
+alias skippable=chore-skippable
+alias unk=chore-unknown-duration
