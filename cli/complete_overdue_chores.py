d666e5cdbca641582c7e64454763f206a4b67e2e
Karl Dickman <karldickman@gmail.com>
2024-09-22 15:05:17 -0700

    Add script to complete all overdue chores.

---
+++ cli/complete_overdue_chores.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+
+from argparse import ArgumentParser
+from configparser import ConfigParser
+from datetime import datetime
+from os.path import expanduser
+
+import mysql.connector
+
+def parse_arguments():
+    parser = ArgumentParser(description="Complete all overdue chores on the specified date.")
+    parser.add_argument("date", type = lambda d: datetime.strptime(d, "%Y-%m-%d").date(), help = "The date on which to complet the overdue chores.")
+    return parser.parse_args()
+
+def config(filename: str = "~/.my.cnf", section: str = "clientchores") -> "dict[str, str]":
+    filename = expanduser(filename)
+    parser = ConfigParser()
+    parser.read(filename)
+    db_config: "dict[str, str]" = {}
+    if parser.has_section(section):
+        params = parser.items(section)
+        for param in params:
+            key, value = param
+            db_config[key] = str(value)
+        return db_config
+    else:
+        raise Exception(f"Section {section} not found in the {filename} file.")
+
+def database():
+    db_config = config()
+    return mysql.connector.connect(**db_config)
+
+def main():
+    args = parse_arguments()
+    cnx = database()
+    with cnx.cursor() as cursor:
+        cursor.execute("""
+            SELECT chore, overdue_chore_completion_status_id
+            FROM chore_completion_overdue_statuses
+            WHERE due_date < DATE_ADD(%s, INTERVAL 1 DAY)
+                AND DATE(next_due_date) <= DATE_ADD(%s, INTERVAL 1 DAY)
+                AND overdue_chore_completion_status_id IS NOT NULL
+            ORDER BY overdue_chore_completion_status DESC, next_due_date, due_date;
+        """, [args.date, args.date])
+        overdue_chores = cursor.fetchall()
+    for chore in overdue_chores:
+        chore_name, status_id = chore
+        if status_id == 2:
+            print(f"CALL skip_chore('{chore_name}', @c, @n);")
+        elif status_id == 3:
+            print(f"CALL complete_chore_without_data('{chore_name}', NULL, @c, @n);")
+        else:
+            print("/* Skipping", chore_name, "unknown status_id", status_id, "*/")
+
+if __name__ == "__main__":
+    main()
---
+++ sql/views/chore_completion_overdue_statuses.sql
@@ -0,0 +1,17 @@
+CREATE OR REPLACE VIEW chore_completion_overdue_statuses
+AS
+SELECT chore_completion_id
+		, chore_id
+		, chore
+		, due_date
+        , next_due_date
+        , frequency
+        , overdue_chore_completion_status_id
+        , chore_completion_status AS overdue_chore_completion_status
+	FROM chore_completion_next_due_dates
+    JOIN chores USING (chore_id)
+    LEFT JOIN chore_overdue_statuses USING (chore_id)
+    LEFT JOIN chore_completion_statuses
+		ON overdue_chore_completion_status_id = chore_completion_statuses.chore_completion_status_id
+    WHERE chore_completion_next_due_dates.chore_completion_status_id = 1 -- active
+	ORDER BY overdue_chore_completion_status DESC, next_due_date, due_date;
